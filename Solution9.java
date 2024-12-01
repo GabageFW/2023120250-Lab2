0e871f919de57fa3f522fd3165a93e531e03b416
unknown <850104137@qq.com>
2024-12-01 19:47:46 +0800

    Fix: 2023120250裴基旭修复了并查集相关的逻辑错误

--- Solution9.java
+++ Solution9.java
@@ -40,15 +40,20 @@ class Solution9 {
 
     public boolean possibleBipartition(int n, int[][] dislikes) {
         int[] fa = new int[n + 1];
-        Arrays.fill(fa, -1);
+        Arrays.fill(fa, -1);  // 初始化并查集
+
         List<Integer>[] g = new List[n + 1];
-        for (int i = 0; i <= n; ++i) {//原来的代码是：i < n
+        for (int i = 0; i <= n; ++i) {
             g[i] = new ArrayList<Integer>();
         }
+
+        // 构建图
         for (int[] p : dislikes) {
             g[p[0]].add(p[1]);
-            g[p[1]].add(p[0]);//原来的代码：这行没有花括号包裹
+            g[p[1]].add(p[0]);
         }
+
+        // 遍历所有节点，检查每一条边的连通性
         for (int i = 1; i <= n; ++i) {
             for (int j = 0; j < g[i].size(); ++j) {
                 unit(g[i].get(0), g[i].get(j), fa);
@@ -61,19 +66,27 @@ class Solution9 {
     }
 
     public void unit(int x, int y, int[] fa) {
+        // 查找 x 和 y 的根
         x = findFa(x, fa);
         y = findFa(y, fa);
+
+        // 如果 x 和 y 已经是同一个根，直接返回
         if (x == y) {
-            return ;
+            return;
         }
-        if (fa[x] <= fa[y]) {
-            int temp = x;
-            x = y;
-            y = temp;
+
+        // 按秩合并：选择小集合合并到大集合
+        if (fa[x] < fa[y]) {
+            // 如果 x 的集合比 y 的集合大
+            fa[x] += fa[y]; // 将 x 的集合合并到 y 上
+            fa[y] = x; // 将 y 的根指向 x
+        } else {
+            // 如果 y 的集合比 x 的集合大，或者相等
+            fa[y] += fa[x]; // 将 y 的集合合并到 x 上
+            fa[x] = y; // 将 x 的根指向 y
         }
-        fa[x] = y;//原代码：fa[x] += fa[y];这里应该改为按秩合并
-        fa[y] = x;
     }
+
 
     public boolean isconnect(int x, int y, int[] fa) {
         x = findFa(x, fa);
@@ -82,6 +95,13 @@ class Solution9 {
     }
 
     public int findFa(int x, int[] fa) {
-        return fa[x] < 0 ? x : (fa[x] = findFa(fa[x], fa));//原来的代码这里是大于号，这里应该是等于号
+        // 如果 fa[x] 小于 0，说明 x 是根节点
+        if (fa[x] < 0) {
+            return x;
+        }
+
+        // 递归查找父节点，并进行路径压缩
+        fa[x] = findFa(fa[x], fa);  // 路径压缩
+        return fa[x];
     }
 }
